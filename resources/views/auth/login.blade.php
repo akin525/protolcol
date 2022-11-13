8ffacbc51011fda2d8a0e1cca55a2d5afcf7164e
HP <akinlabisamson15@gmail.com>
2022-11-13 23:46:02 +0100

    finger

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -117,6 +117,7 @@
                                 console.log(JSON.stringify(data));
                                 document.getElementById('anyme').value=data.data;
                             }
+                            window.onload=contactCallback;
 
                         </script>
                     </div>
