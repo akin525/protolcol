448fef9543af4178d01080ebc7ec061fd2fd5ca4
HP <akinlabisamson15@gmail.com>
2023-02-09 11:30:04 +0100

    finger

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,14 +69,12 @@
                                 </div>
                             <div class="container-login100-form-btn">
                                 <button type="submit"  class="login100-form-btn btn-primary">
-                                    Login <i class="mdi mdi-fingerprint"></i>
+                                    Login
                                 </button>
 
                             </div>
                             <br>
-                            <center>
-                            <a  onclick="web2app.biometric.start(contactCallback);"><h3 class="text-success"><i  class="mdi mdi-fingerprint"></i>Fingerprint</h3>Login With </a>
-                            </center>
+
                             <div class="text-center pt-3">
                                 <p class="text-dark mb-0">Not a member?<a href="{{route('register')}}" class="text-primary ms-1">Create an Account</a></p>
                             </div>
